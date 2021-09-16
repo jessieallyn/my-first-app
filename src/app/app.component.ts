41662501426266052bd939649e16fd4036c6e189
Jessie Woods <jessieallyn@gmail.com>
2021-09-16 14:32:40 -0500

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-exercise';
+  game = "Pandemic";
+  usergame = "";
+  whichGame() {
+    if (this.game.length <= this.usergame.length) {
+      console.log("Ok, let's play" + this.usergame)
+    }
+    else {
+      console.log("Ok, we're going to play" + this.game + "anyways")
+    }
+
+  }
 }
